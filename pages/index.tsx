dd69e9bd0aa064459ce1716f82905d32a8672976
dongminjang <dongminjang@dongminjangui-iMac.local>
2021-07-15 01:10:57 +0900

    Add images in index page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,8 @@
-import { useEffect } from 'react';
+
+import { jsx, css } from '@emotion/react';
 import Head from 'next/head'
 
 const Home: React.FC = () => {
-  useEffect(() => {
-    // setTimeout(() => {
-    //   window.location.href="https://dongmin-jang.medium.com";
-    // }, 2000);
-  }, []);
   return (
     <div>
       <Head>
@@ -14,9 +10,79 @@ const Home: React.FC = () => {
         <meta name="description" content="Record yourself just for you" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0, viewport-fit=cover" />
       </Head>
-      <div>
-        채용, 외주, 라운딩, 컨텐츠 문의는 아래 메일로 문의 바랍니다.
-        jdm85kor@gmail.com
+      <div css={css`
+        margin: 0 auto;
+        padding: 0 50px;
+        width: 100%;
+        max-width: 1024px;
+        box-sizing: border-box;
+        
+      `}>
+        <div css={css`
+          display: flex;
+          height: 500px;
+        `}>
+          <div css={css`
+            margin: 50px;
+          `}>
+            <div css={css`
+              margin: 0 auto;
+              width: 362px;
+              height: 350px;
+              background: no-repeat center center url("letter.png");
+            `} />
+            <div css={css`
+              height: 50px;
+              text-align: center;
+            `}>Job offer</div>
+          </div>
+          <div css={css`
+            margin: 50px;
+          `}>
+            <div css={css`
+              width: 362px;
+              height: 350px;
+              background: no-repeat center center url("develop.png");
+            `} />
+            <div css={css`
+              height: 50px;
+              text-align: center;
+            `}>Outsourcing development</div>
+          </div>
+        </div>
+        <div css={css`
+          display: flex;
+          justify-content: space-between;
+        `}>
+          <div css={css`
+            margin: 50px;
+          `}>
+            <div css={css`
+              display: inline-block;
+              width: 362px;
+              height: 350px;
+              background: no-repeat center center url("partnership.png");
+            `} />
+            <div css={css`
+                height: 50px;
+                text-align: center;
+              `}>Contents partnership</div>
+          </div>
+          <div css={css`
+            margin: 50px;
+          `}>
+            <div css={css`
+              display: inline-block;
+              width: 362px;
+              height: 350px;
+              background: no-repeat center center url("golf.png");
+            `} />
+            <div css={css`
+                height: 50px;
+                text-align: center;
+              `}>Play golf</div>
+          </div>
+        </div>
       </div>
     </div>
   )
