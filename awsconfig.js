4791b75753d0e2fbe4e012d68471d0cd4e9037e1
jdm <jdm85kor@gmail.com>
2021-09-12 15:33:56 +0900

    Add amplify

---
+++ awsconfig.js
@@ -0,0 +1,10 @@
+// import/no-anonymous-default-export
+const config = {
+  Auth: {
+    region: "ap-northeast-2",
+    userPoolId: "ap-northeast-2_71XuGrE06",
+    userPoolWebClientId: "3kvrs1co551opb4tr7qd6qde4a"
+  }
+};
+
+export default config;
--- pages/playground/index.tsx
+++ pages/playground/index.tsx
@@ -1,7 +1,15 @@
+import Amplify from "aws-amplify";
+import { AmplifyAuthenticator } from "@aws-amplify/ui-react";
+import awsconfig from "../../awsconfig";
+
+Amplify.configure(awsconfig);
+
 const Playground: React.FC = () => {
-  
   return (
     <main>
+      <AmplifyAuthenticator>
+        <div>loggedIn!!</div>
+      </AmplifyAuthenticator>
       You can&apos;t access this page
     </main>
   );
