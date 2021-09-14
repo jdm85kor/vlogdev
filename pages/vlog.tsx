73b6c190675962c05e794c06b6838355edddc1da
Dongmin Jang <jdm85kor@gmail.com>
2021-09-14 10:03:45 +0900

    Update vlog.tsx

--- pages/vlog.tsx
+++ pages/vlog.tsx
@@ -1,5 +1,4 @@
 import { useEffect } from 'react';
-import Head from 'next/head'
 
 const About: React.FC = () => {
   useEffect(() => {
@@ -9,12 +8,10 @@ const About: React.FC = () => {
   }, []);
   return (
     <div>
-      <Head>
-        <title>Video</title>
-        <meta name="description" content="Show me your records" />
-      </Head>
       작업 중입니다.
       youtube로 이동 됩니다.
+      <title>Video</title>
+      <meta name="description" content="Show me your records" />
     </div>
   );
 };
