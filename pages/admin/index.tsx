f586b1f3c981b82c324389690e3ba970a5035d4c
jdm <jdm85kor@gmail.com>
2021-09-14 22:03:29 +0900

    Modify admin

--- pages/admin/index.tsx
+++ pages/admin/index.tsx
@@ -1,4 +1,5 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useMemo } from 'react';
+import Head from 'next/head'
 import { css } from '@emotion/react';
 import { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react';
 import { Auth, Hub } from 'aws-amplify';
@@ -26,26 +27,37 @@ const Admin: React.FC = () => {
     getUser().then(userData => setUser(userData));
   }, []);
 
-  function getUser() {
+  const getUser = () => {
     return Auth.currentAuthenticatedUser()
       .then(userData => userData)
       .catch(() => console.log('Not signed in'));
-  }
+  };
+
+  const isAdmin = useMemo((): boolean => {
+    const groups = (user as any)?.signInUserSession?.accessToken?.payload["cognito:groups"] || [];
+    return groups.includes('admin');
+  }, [user]);
 
   return (
     <div css={css`
       margin: 0 auto;
       max-width: 1920px;
       `}>
+        <Head>
+          <title>VLOG admin</title>
+        </Head>
         <div css={css`
         margin: 50px;
         `}>
           Admin Page
-          { user &&
+          { isAdmin ?
             <p>
-              provider name: {((user as any).attributes?.identities || [{}])[0]?.providerName || ''}<br />
+              provider name: {JSON.parse((user as any)?.attributes?.identities || '[{}]')[0]?.providerName || 'none'}<br />
               username: {(user as any)?.username || ''}<br />
-              email: {(user as any).attributes.email}
+              email: {(user as any)?.attributes.email}
+            </p> : 
+            <p>
+              This page is for admin.
             </p>
           }
           {user ? (
