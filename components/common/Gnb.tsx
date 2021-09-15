103129657696555047a834edb104a8d5af8e4981
Dongmin Jang <jdm85kor@gmail.com>
2021-09-15 16:15:46 +0900

    Update Gnb.tsx

--- components/common/Gnb.tsx
+++ components/common/Gnb.tsx
@@ -64,7 +64,7 @@ const Gnb: React.FC = () => {
       <Nav>
         <Link href="/" passHref>
           <A>
-            <Img src="https://d6c63ppcwec2x.cloudfront.net/logo144.png" alt="" width="30" height="30" />
+            <Img src="https://d6c63ppcwec2x.cloudfront.net/logo_96x96.png" alt="" width="30" height="30" />
           </A>
         </Link>
         <Ul>
