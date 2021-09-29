477c95a8e2eec63fa1e647ee07ae5ad121bee32b
jdm <jdm85kor@gmail.com>
2021-09-30 08:24:19 +0900

    Add latest video

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,6 +4,7 @@ import { css } from '@emotion/react';
 import Head from 'next/head'
 import Loading from "@components/common/Loading";
 import { mq, colors } from '@styles/theme';
+import { apiCall } from '@utils/apis';
 
 const sectionStyle = css`
   margin: 0 auto 20px;
@@ -31,10 +32,6 @@ const buttonStyle = css`
   border: 1px solid #fff;
   border-radius: 10px;
   cursor: pointer;
-
-  &:hover {
-    border-color: ${colors.hermes}
-  }
 `;
 
 const imgStyle = (src: string) => css`
@@ -53,11 +50,45 @@ const imgTitleStyle = css`
   font-size: 24px;
   color: #000;
 `;
+const thumbnailStyle = (thumbnails: Record<'default' | 'high' | 'high', {
+  width: number;
+  height: number;
+  url: string;
+}>) => css`
+  display: inline-block;
+  width: 100px;
+  height: 100px;
+  ${mq({
+    background: [
+      `no-repeat 100%/contain url(${thumbnails.default.url})`,
+      `no-repeat 100%/contain url(${thumbnails.default.url})`,
+      `no-repeat 100%/contain url(${thumbnails.high.url})`
+    ]
+  })}
+`;
+const titleStyle = css`
+  display: inline-block;
+  margin-top: 5px;
+  width: 100%;
+  overflow: hidden;
+  text-overflow: ellipsis;
+  white-space: nowrap;
+`;
 
 const Home: React.FC = () => {
   const [isShowRequest, setIsShowRequest] = useState(false);
+  const [latestVideo, setLatestVideo] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
   const [type, setType] = useState<string>('');
+
+  const fetchLatestVideo = useCallback(async () => {
+    const { data } = await apiCall({
+      method: 'get',
+      url: '/vlogdev/video',
+      query: { type: 'latest' },
+    });
+    setLatestVideo(data.items);
+  }, []);
 
   const handleClickButton = useCallback((t: string) => {
     setIsShowRequest(true);
@@ -68,8 +99,9 @@ const Home: React.FC = () => {
     const timer = setTimeout(() => {
       setIsLoading(false);
     }, 3000);
+    fetchLatestVideo();
     return () => clearTimeout(timer);
-  } ,[]);
+  } ,[fetchLatestVideo]);
   
   return (
     <main css={css`
@@ -89,10 +121,55 @@ const Home: React.FC = () => {
             Record yourself. Record do something. Record whatever.
           </h1>
           <section css={sectionStyle}>
+            <div css={backgroundImg('pungmu_morning_s.jpg')} />
             <h2>
               New Videos
             </h2>
-            <div css={backgroundImg('pungmu_morning_s.jpg')} />
+            <ul css={css`
+              margin: 0;
+              padding: 0;
+              list-style: none;
+              width: 100%;
+              height: 140px;
+              overflow-x: auto;
+              white-space: nowrap;
+            `}>
+              {
+                latestVideo.map((v: any) => (
+                  <li
+                    key={v.videoId}
+                    css={css`
+                      display: inline-flex;
+                      flex-direction: column;
+                      width: 114px;
+                      text-align: center;
+                      padding: 2px;
+                      border-radius: 10px;
+                      box-sizing: border-box;
+                    `}
+                  >
+                    <a
+                      css={css`
+                        display: inline-flex;
+                        flex-direction: column;
+                        padding: 0;
+                        background: inherit;
+                        border: none;
+                        cursor: pointer;
+                        text-decoration: none;
+                        color: #555;
+                      `}
+                      href={`https://www.youtube.com/watch?v=${v.videoId}`}
+                    >
+                      <div css={thumbnailStyle(v.thumbnails)} />
+                      <span css={titleStyle}>
+                        { v.videoTitle }
+                      </span>
+                    </a>
+                  </li>)
+                )
+              }
+            </ul>
           </section>
           <section css={sectionStyle}>
             <h2>
