50ec8ea0f74979f01fc29865cb10259735d2e4f6
jdm <jdm85kor@gmail.com>
2021-10-06 14:06:56 +0900

    Modify Vlog state to mobx

--- mobx/store.ts
+++ mobx/store.ts
@@ -1,9 +1,12 @@
 import User from './user';
+import Vlog from './vlog';
 
 export default class Store {
   user;
+  vlog;
 
   constructor() {
     this.user = new User();
+    this.vlog = new Vlog();
   }
 }
