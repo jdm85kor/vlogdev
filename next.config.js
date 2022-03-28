6aed6525db5330ec4adb409fc3c2eed2bed806ad
jdm85kor <jdm85kor@gmail.com>
2022-03-29 08:19:21 +0900

    feat: add next transpile module

--- next.config.js
+++ next.config.js
@@ -1,15 +1,20 @@
-const isProd = process.env.NODE_ENV === 'production'
+const withTM = require('next-transpile-modules')(['@babylonjs/core']);
+const withPlugins = require('next-compose-plugins');
 
-module.exports = {
+const isProd = process.env.NODE_ENV === 'production';
+
+const nextConfig = {
   assetPrefix: isProd ? 'https://d9x1g5sz86pe3.cloudfront.net' : '',
   reactStrictMode: true,
   trailingSlash: true,
   webpack(config) {
     config.module.rules.push({
       test: /\.svg$/,
-      use: ["@svgr/webpack"]
+      use: ['@svgr/webpack'],
     });
 
     return config;
   },
-}
+};
+
+module.exports = withPlugins([withTM], nextConfig);
