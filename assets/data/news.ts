0cf1355601d909585f3c29e2d71bdc3fc4457aa3
jdm <jdm@jdmui-iMac.local>
2023-03-19 11:58:08 +0900

    update posts

---
+++ assets/data/news.ts
@@ -0,0 +1,40 @@
+export const mainNews = [
+  {
+    url: 'https://devblogs.microsoft.com/typescript/announcing-typescript-5-0',
+    title: 'Typescript 5 release',
+  },
+  {
+    url: 'https://cdn.openai.com/papers/gpt-4.pdf',
+    title: 'GPT-4 Technical Report',
+  },
+  {
+    url: 'https://academy.cs.cmu.edu/',
+    title: 'CMU Computer science curriculum',
+  },
+  {
+    url: 'https://huyenchip.com/2023/01/24/what-we-look-for-in-a-candidate.html?fbclid=IwAR39p8kqxea0bmsn6etYuZX4WZjGLu42Tksa_D6spsmRdc-dp95geSmxb7I',
+    title: 'What we look for in a resume',
+  }
+];
+
+// archive
+  // {
+  //   url: 'https://news.hada.io/weekly/202122',
+  //   title: '재미로 만든 작은 인터넷 프로젝트 팔기',
+  // },
+  // {
+  //   url: 'https://dev.to/o1lab/how-my-open-source-project-got-angel-investment-from-its-user-12dp',
+  //   title: '내 오픈소스 프로젝트가 유저로 부터 엔젤 투자를 받는 방법',
+  // },
+  // {
+  //   url: 'http://channy.creation.net/blog/1443',
+  //   title: '스타트업의 개발자 채용 비법',
+  // },
+  // {
+  //   url: 'https://web.dev/bundling-non-js-resources/',
+  //   title: 'Bundling non-JavaScript resources',
+  // },
+  // {
+  //   url: 'https://github.com/jongman/articles/wiki/solving-every-sudoku-puzzle#%EC%8A%A4%EB%8F%84%EC%BF%A0-%EC%9A%A9%EC%96%B4%EC%99%80-%EC%9E%90%EB%A3%8C-%EA%B5%AC%EC%A1%B0-%ED%91%9C%ED%98%84',
+  //   title: 'sudoku 문제 풀기',
+  // },
