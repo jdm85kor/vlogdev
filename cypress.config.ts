1a1b7a0e514a9e700a9d62a9d369522d631eb416
jdm <jdm@jdmui-iMac.local>
2023-07-23 00:30:41 +0900

    complite cypress env

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,7 +1,6 @@
 import {defineConfig } from 'cypress';
 
 export default defineConfig({
-  projectId: process.env.NEXT_PUBLIC_CYPRESS_PROJECT_ID,
   e2e: {
     baseUrl: 'http://localhost:3000',
   },
