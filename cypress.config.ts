11b9955cb781c6d4f269fe0f91552fc2c8e1e7ac
jdm <jdm@jdmui-iMac.local>
2023-07-23 01:52:08 +0900

    modify test codes

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,13 +1,22 @@
 import {defineConfig } from 'cypress';
+import path from 'path';
 
 export default defineConfig({
   e2e: {
-    baseUrl: 'http://localhost:3000',
+    baseUrl: 'http://localhost:4000',
   },
   component: {
     devServer: {
       framework: 'next',
       bundler: 'webpack',
+      webpackConfig: {
+        resolve: {
+          alias: {
+            '@components': path.resolve(__dirname, './components'),
+            '@containers': path.resolve(__dirname, './containers'),
+          },
+        },
+      }
     },
   },
 } as any)
