823d3f034343fd365981091c4afc5bb4b1168aae
jdm <jdm@jdmui-iMac.local>
2023-07-23 01:58:58 +0900

    modify env

--- cypress.config.ts
+++ cypress.config.ts
@@ -3,7 +3,7 @@ import path from 'path';
 
 export default defineConfig({
   e2e: {
-    baseUrl: 'http://localhost:4000',
+    baseUrl: 'http://localhost:3000',
   },
   component: {
     devServer: {
--- cypress/integration/test.spec.jsx
+++ cypress/integration/test.spec.jsx
@@ -1,11 +1,11 @@
 describe('Enter page', () => {
   it('visible text', () => {
-    cy.visit('http://localhost:4000');
+    cy.visit('http://localhost:3000');
     cy.get('h2:first-of-type').should('have.text', 'New VideosNew blog postsTo manager');
   });
 
   it('How many anchor tag are there on the About', () => {
-    cy.visit('http://localhost:4000/about');
+    cy.visit('http://localhost:3000/about');
     cy.get('a').should('have.length', 17);
   });
 });
