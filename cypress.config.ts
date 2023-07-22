ea53057c2d90b02bc12724585bde3a562c9b17f1
jdm <jdm@jdmui-iMac.local>
2023-07-23 00:10:39 +0900

    fix config

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,7 +1,7 @@
 import {defineConfig } from 'cypress';
 
 export default defineConfig({
-  projectId: process.env.CYPRESS_PROJECT_ID,
+  projectId: process.env.NEXT_PUBLIC_CYPRESS_PROJECT_ID,
   e2e: {
     baseUrl: 'http://localhost:3000',
   },
